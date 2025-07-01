5cb6682a7a5357b3e1054e33e9b62b5dd9be2c42
SURFACEPRO9\mitsu <mitsuhiro.seino@gmail.com>
2025-07-01 11:08:10 +0900

    rollup4.40

---
+++ src/Keys.test.ts
@@ -0,0 +1,37 @@
+import Keys from './Keys';
+
+type TestProps = {
+  str: string;
+  num: number;
+  date: Date;
+  bool: boolean;
+};
+
+type RequiredProps = {
+  str: string;
+  num: number;
+  date: Date;
+  bool: boolean;
+  obj: TestProps;
+  arr: TestProps[];
+};
+
+describe('Keys', () => {
+  test('全て', () => {
+    type FullKeys = Keys<RequiredProps>;
+    const str: FullKeys = 'str';
+    const num: FullKeys = 'num';
+    const date: FullKeys = 'date';
+    const bool: FullKeys = 'bool';
+    const obj: FullKeys = 'obj';
+    const arr: FullKeys = 'arr';
+    expect(true).toBe(true);
+  });
+  test('一部', () => {
+    type PartialKeys = Keys<RequiredProps, string | number | Date>;
+    const str: PartialKeys = 'str';
+    const num: PartialKeys = 'num';
+    const date: PartialKeys = 'date';
+    expect(true).toBe(true);
+  });
+});
---
+++ src/TypeOfResult.ts
@@ -0,0 +1,14 @@
+/**
+ * typeofの結果の型
+ */
+type TypeOfResult =
+  | 'undefined'
+  | 'object'
+  | 'boolean'
+  | 'number'
+  | 'bigint'
+  | 'string'
+  | 'symbol'
+  | 'function';
+
+export default TypeOfResult;
